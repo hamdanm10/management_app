4001cefae32273884a7272de60b1add4ffbbee47
hamdanm10 <midunhamdan@gmail.com>
2025-08-07 17:41:05 +0700

    modifying classes

--- app/controllers/super_admin/unit_types_controller.rb
+++ app/controllers/super_admin/unit_types_controller.rb
@@ -1,4 +1,9 @@
 class SuperAdmin::UnitTypesController < SuperAdminApplicationController
   def index
+    limit = RecordLimit.call(params[:limit])
+
+    @q = UnitType.ransack(params[:q])
+    @unit_types = @q.result.order(name: :asc)
+    @pagy, @unit_types = pagy(@unit_types, limit:)
   end
 end
--- app/models/unit_type.rb
+++ app/models/unit_type.rb
@@ -1,4 +1,8 @@
 class UnitType < ApplicationRecord
   validates :name, presence: true
   validates :note, presence: false
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["name"]
+  end
 end
