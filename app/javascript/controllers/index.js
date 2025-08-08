891d870cff50703b4cd36fd41fcbad3273a8021b
hamdanm10 <midunhamdan@gmail.com>
2025-08-08 11:40:40 +0700

    adding notification

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,10 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application";
+import { application } from "./application"
 
-import RecordLimitSelectorController from "./record_limit_selector_controller";
-application.register("record-limit-selector", RecordLimitSelectorController);
+import NotificationController from "./notification_controller"
+application.register("notification", NotificationController)
+
+import RecordLimitSelectorController from "./record_limit_selector_controller"
+application.register("record-limit-selector", RecordLimitSelectorController)
