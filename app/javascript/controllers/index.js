1f662aaa68b0868fc29741934e1d507e9afacb02
hamdanm10 <midunhamdan@gmail.com>
2025-08-08 15:03:09 +0700

    adding dialog stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import DialogController from "./dialog_controller"
+application.register("dialog", DialogController)
+
 import NotificationController from "./notification_controller"
 application.register("notification", NotificationController)
 
