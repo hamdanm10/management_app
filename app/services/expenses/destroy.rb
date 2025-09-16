6af98ad5e38fbef8b9e5028d40df234be72d8c58
hamdanm10 <midunhamdan@gmail.com>
2025-09-16 13:15:38 +0700

    modifying expense service

--- app/services/expenses/destroy.rb
+++ app/services/expenses/destroy.rb
@@ -1,9 +1,20 @@
 class Expenses::Destroy < ApplicationService
   def call(expense:)
-    if expense.destroy
-      success(expense: expense)
-    else
-      failure(expense: expense)
+    expense_items = expense.expense_items
+    
+    old_total_price = expense_items.sum { |expense_item| expense_item.total_price }
+
+    ActiveRecord::Base.transaction do
+      expense.destroy!
+
+      finance = Finance.first
+
+      finance.production_cost += old_total_price
+
+      finance.save!
     end
+  rescue ActiveRecord::RecordInvalid => e
+    failure(expense: expense)
   end
 end
+  
